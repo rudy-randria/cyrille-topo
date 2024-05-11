138a9f6094b0f6ce3b50bef5f510753beacaa832
Liva Fitia Samuelsa <fitianohavinirina4@gmail.com>
2024-05-12 00:26:40 +0300

    recuperation de nom de l'entite correspondant

--- models/UserModel.php
+++ models/UserModel.php
@@ -9,9 +9,11 @@ class UserModel {
         // Si les informations d'identification sont valides, retournez true, sinon retournez false
         
         global $db;
-        $stmt = $db->prepare("SELECT id_user, nom, prenom, tel, mail, poste, login, mot_de_passe, id_entite
+        $stmt = $db->prepare("SELECT id_user, nom, prenom, tel, mail, poste, login, mot_de_passe, e.name
             FROM 
-                public.user_lp
+                public.user_lp u
+            JOIN 
+                entity e ON u.id_entity = e.id_entity   -- structure de database à modifier, remplacer id_entite à id_entity dans la table user_lp
             WHERE
                 login =:login AND  mot_de_passe = :mdp
         ");
@@ -26,6 +28,7 @@ class UserModel {
             session_start();
             $_SESSION['id'] = $user['id_user'];
             $_SESSION['mdp'] = $user['mot_de_passe'];
+            $_SESSION['entite'] = $user['name'];
             return true;
         } else {
             return false;
